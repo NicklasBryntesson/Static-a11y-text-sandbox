6751f52033fdaa202332cc817dd02c9340e99094
Nicklas Bryntesson <nicklas.bryntesson@dazy.se>
2023-04-11 08:47:39 +0200

    js & base in place

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,35 @@
 const documentBody = document.querySelector('body');
 console.log(documentBody);
+
+// Track the browser fontsize
+const remBox = document.getElementById('remBox');
+const remText = document.getElementById('remValue');
+
+const ro = new ResizeObserver(entries => {
+
+  entries.forEach(entry => {
+
+    const fontSize = parseFloat(window.getComputedStyle(entry.target).fontSize);
+
+    remBox.parentElement.style.fontSize = fontSize + 'px';
+
+    const remWidth = window.getComputedStyle(remBox).width;
+    remValue.innerHTML = remWidth;
+  });
+});
+
+ro.observe(remBox.parentElement);
+
+
+
+// Switch between responsive values
+const responsiveValuesSwitch = document.getElementById('responsiveValuesSwitch');
+
+if (responsiveValuesSwitch) {
+
+  responsiveValuesSwitch.addEventListener('change', function(event) {
+    const newMeasure = event.target.value;
+    document.documentElement.setAttribute('data-measure', newMeasure);
+  });
+
+}
